7df39e0fde9e397af07c18eeb75ecaefd3095051
ashraffares <ashraffares090@gmail.com>
2021-03-30 10:25:14 +0200

    Fix rubocop error spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :user_name , presence:true
+  validates :user_name, presence: true
   has_many :posts
 end
